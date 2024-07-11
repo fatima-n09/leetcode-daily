02292502fb4e88fee912e87c2acd24cfed4747e5
fatima-n09 <150540933+fatima-n09@users.noreply.github.com>
2024-07-11 17:07:40 +0500

    Time: 0 ms (100.00%) | Memory: 10.1 MB (5.48%) - LeetSync

---
+++ 1833-find-the-highest-altitude/find-the-highest-altitude.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        int currentAltitude=0;
+        int maxAltitude=0;
+        for( int g: gain)
+        {
+        currentAltitude += g;
+        maxAltitude=max(maxAltitude,currentAltitude);
+        }
+        return maxAltitude;
+    }
+};
