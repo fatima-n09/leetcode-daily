ec05a647821efbe90fef1ae32b87e3abf7540042
Nur Fatima <150540933+fatima-n09@users.noreply.github.com>
2024-08-19 01:09:16 +0500

    Time: 111 ms (5.77%) | Memory: 97.8 MB (40.58%) - LeetSync

---
+++ 2836-neither-minimum-nor-maximum/neither-minimum-nor-maximum.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int findNonMinOrMax(vector<int>& nums) {
+        if (nums.size() <= 2) {
+            return -1;
+        }
+        
+        insertionSort(nums);
+
+        return nums[1]; 
+    }
+
+private:
+    void insertionSort(vector<int>& nums) {
+        int n = nums.size();
+        for (int i = 1; i < n; ++i) {
+            int temp = nums[i];
+            int j;
+            for (j = i; j > 0 && nums[j - 1] > temp; --j) {
+                nums[j] = nums[j - 1];
+            }
+            nums[j] = temp;
+        }
+    }
+};
