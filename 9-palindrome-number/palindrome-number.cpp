27bcc90c84c9600084bfcba21734e1df082c287d
Nur Fatima <150540933+fatima-n09@users.noreply.github.com>
2024-08-21 21:55:22 +0500

    Time: 8 ms (60.08%) | Memory: 8.5 MB (34.64%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
-    bool isPalindrome(int x) {
-       if(x < 0) return false;
+    bool isPalindrome(int x)
+    {
+       if( x < 0) return false;
        int original = x;
        long reversed = 0;
-       while(x>0)
+       while( x > 0)
        {
-       reversed = reversed * 10 + x % 10;;
-        x /= 10;
+         reversed = reversed*10 +x %10;
+         x /= 10;
        }
-       if(original == reversed){
-        return true;}
-        else{
+        if( original == reversed)
+        {
+            return true;
+        } 
+        else
+        {
             return false;
         }
     }
