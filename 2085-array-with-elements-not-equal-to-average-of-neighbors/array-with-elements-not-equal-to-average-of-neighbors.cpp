cfc5baaaebf90203474d103667797b42510fba49
Nur Fatima <150540933+fatima-n09@users.noreply.github.com>
2024-10-14 03:08:37 +0500

    Time: 172 ms (54.07%) | Memory: 127 MB (66.85%) - LeetSync

---
+++ 2085-array-with-elements-not-equal-to-average-of-neighbors/array-with-elements-not-equal-to-average-of-neighbors.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) 
+    {
+      vector<int> ans;
+      sort(nums.begin(),nums.end());
+        for(int i=1;i<nums.size()-1;i++){
+            int a=nums[i-1];
+            int b=nums[i];
+            int c=nums[i+1];
+            if(a>b && b>c || a<b && b<c)
+            {
+                swap(nums[i],nums[i+1]);
+            }
+        }
+        return nums;   
+    }
+};
