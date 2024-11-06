8356946ae5a719043bc53b16edb5a9584ee5fa3e
Nur Fatima <150540933+fatima-n09@users.noreply.github.com>
2024-11-07 03:27:19 +0500

    Time: 34 ms (7.91%) | Memory: 34.3 MB (10.63%) - LeetSync

--- 3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.cpp
+++ 3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.cpp
@@ -2,19 +2,19 @@
 public:
     bool canSortArray(vector<int>& nums) {
         vector<int> setBits;
-        map<int, int> map;
+        map<int, int> mp;
         for (auto num : nums) {
             int count = 0;
             for (int i = 0; i < 31; i++) {
                 if ((num & 1 << i))
                     count++;
             }
-            map[num] = count;
+            mp[num] = count;
         }
         for (int i = 0; i < nums.size(); i++) {
             for (int j = i + 1; j < nums.size(); j++) {
                 if (nums[i] > nums[j]) {
-                    if (map[nums[i]] != map[nums[j]])
+                    if (mp[nums[i]] != mp[nums[j]])
                         return false;
                 }
             }
