c8374862189555b0a51520662760df2ebb006c0e
Nur Fatima <150540933+fatima-n09@users.noreply.github.com>
2025-07-03 06:33:03 +0500

    Time: 1023 ms (6.94%) | Memory: 60.4 MB (9.20%) - LeetSync

---
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        int len1 = s1.length(); 
+        int len2 = s2.length();
+        if (len1 > len2) return false;
+        sort(s1.begin(), s1.end());
+        for (int i = 0; i <= len2 - len1; i++) {
+            string window = s2.substr(i, len1);
+            sort(window.begin(), window.end());
+            if (window == s1) return true;
+        }
+        return false;
+    }
+};
