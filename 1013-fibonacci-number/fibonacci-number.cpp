d854d8e8d04249eac1b391d2acb94655870a95be
Nur Fatima <150540933+fatima-n09@users.noreply.github.com>
2025-08-07 23:05:02 +0500

    Time: 0 ms (100.00%) | Memory: 7.8 MB (64.26%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -2,12 +2,15 @@ class Solution {
 public:
     int fib(int n) {
         if( n <= 1)
-        {
-            return n;
-        }
-        else
-        {
-            return fib(n - 1) + fib(n - 2);
-        }
+        return n;
+        int prev1= 0;
+        int prev2= 1;
+       for(int i = 2; i<= n; i++){
+        //curr block-scope variable 
+        int curr = prev1 + prev2;
+        prev1= prev2;
+        prev2 = curr;
+       }
+       return prev2;
     }
 };
